d768cd61dd292da2d96ce875506ee0a93105775f
winstxnhdw <winston.ho@redbulltechnology.com>
2025-03-18 10:01:26 +0000

    feat: upgrade to Granian v2

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,5 @@
-from granian import Granian
 from granian.constants import Interfaces
+from granian.server import Server
 
 from server.app import app
 from server.config import Config
@@ -11,12 +11,12 @@ def main() -> None:
     -------
     programmatically run the server with Granian
     """
-    granian = Granian(
+    granian = Server(
         f'{app.__module__}:{app.__name__}',
-        '0.0.0.0',
-        Config.server_port,
-        Interfaces.ASGI,
-        Config.worker_count,
+        address='0.0.0.0',
+        port=Config.server_port,
+        interface=Interfaces.ASGI,
+        workers=Config.worker_count,
         log_access=True,
         log_access_format='[%(time)s] %(status)d "%(method)s %(path)s %(protocol)s" %(addr)s in %(dt_ms).2f ms',
         url_path_prefix=Config.server_root_path,
